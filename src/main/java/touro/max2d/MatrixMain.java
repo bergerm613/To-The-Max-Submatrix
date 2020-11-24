eef43c67544ae39547a51def8f46f6a50ab9cd75
MB <bergerm613@gmail.com>
2020-11-24 18:20:51 -0500

    added catch for empty args

--- src/main/java/touro/max2d/MatrixMain.java
+++ src/main/java/touro/max2d/MatrixMain.java
@@ -2,11 +2,17 @@ package touro.max2d;
 
 public class MatrixMain {
     public static void main(String[] args) {
-        MatrixInputReader inputReader = new MatrixInputReader(args[0]);
+        if (args.length == 0) {
+            System.out.println("Please give a filename as argument.");
+        }
+        else {
+            MatrixInputReader inputReader = new MatrixInputReader(args[0]);
 
-        Matrix matrix = new Matrix(inputReader.getMatrix());
-        int maxSum = matrix.getMaxSum();
+            Matrix matrix = new Matrix(inputReader.getMatrix());
+            int maxSum = matrix.getMaxSum();
 
-        System.out.println("Max sum for matrix: " + maxSum);
+            System.out.println("Max sum for matrix: " + maxSum);
+        }
+
     }
 }
